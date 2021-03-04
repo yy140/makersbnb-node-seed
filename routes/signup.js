ea21ddd92ec368df45fe3c8e466f1497221575f2
yy140 <74052695+yy140@users.noreply.github.com>
2021-03-04 11:25:24 +0000

    users successfully added to database

--- routes/signup.js
+++ routes/signup.js
@@ -11,20 +11,41 @@ router.get("/new", async function (req, res, next) {
   res.render("new");
 })
 
-router.post("/new", (req, res) => {
-  const fname = request.body.fname
-  const lname = request.body.lname
-  return Users.create({
-    First_Name: fname,
-    Last_Name: lname,
-}).then(function (users) {
-    if (Users) {
-        response.send(Users);
-    } else {
-        response.status(400).send('Error in insert new record');
-    }
-    res.render("index");
-});
-})
+// router.post("/new", (req, res) => {
+//     User.create({
+//     username: req.body.username,
+//     email: req.body.email,
+//     password: req.body.password 
+// }).then(function (User) {
+//     if (User) {
+//         response.send(User);
+//     } else {
+//         response.status(400).send('Error in insert new record');
+//     }
+//     res.render("index");
+// });
+// //res.render("signup")
+
+
+// })
+router.post('/new', function(req, res) {
+  if (!req.body.email || !req.body.password) {
+      res.status(400).send({
+          status: false,
+          message: ''
+      });
+  } else {
+      User.create({
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+      }).then((User) => res.status(201).send(User)).catch((error) => {
+          console.log(error);
+          res.status(400).send(error);
+          
+      });
+  }
+  res.redirect("/signup"); 
+  });
 
 module.exports = router;
